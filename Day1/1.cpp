3279196e0784ecb3182a7468d79ac2dd83fbf390
Maths Lover <redhacks@protonmail.com>
2019-08-03 22:41:24 +0530

    Added Solution for Day 1 Question

--- Day1/1.cpp
+++ Day1/1.cpp
@@ -1,1 +1,29 @@
+# include <stdio.h>
+#define MAX 100000
 
+void checkPairs(int array[], int array_size, int key)
+{
+  int i, temp;
+  int hashTable[MAX] = {0};
+	for(i = 0; i < array_size; i++)
+	{
+		temp = key - array [i];
+		if (temp >= 0 && hashTable[temp] == 1)
+			printf("Elements are present\n");
+
+		hashTable[ array[i] ] = 1;
+	}
+}
+
+
+
+int main()
+{
+    int array[] = {1,2,3,4,5,6};
+    int key = 11;
+    int array_size = 6;
+
+	checkPairs(array, array_size, key);
+
+     return 0;
+}
